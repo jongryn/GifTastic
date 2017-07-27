d26bf66460a8d79ba919d030004b016757a026e2
Jonathan Gryn <jonathangryn@Jonathans-MacBook-Pro.local>
2017-07-27 13:21:56 -0700

    updated files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,57 +4,96 @@
 // Revisions: Jon (7/23/17) - Added JS
 */
 
-var charactersArray = ["Luke Skywalker", "Obi Wan Kenobi", "Princess Laya", "Han Solo", "R2D2"];
+// create an array of characters - in this case, Star Wars Characters
+var characters = ["Anakin Skywalker", "Yoda", "Leia Organa", "Luke Skywalker", "Kylo Ren", "boba Fett", "Clone tropper", "Pademe Amidala", "Han Solo", "R2-D2"];
 
-$(document).ready(function() {
-  for (var i = 0; i < charactersArray.length; i++) {
-  	$("#character-buttons").append("<button type='button' onclick='searchGif(\"" + charactersArray[i] + "\")' class='btn btn-primary' value=' " + charactersArray[i] + "'> " + charactersArray[i] + " </button>");
+// creates buttons for each of these
+function makeButtons(){
+  
+  // deletes the characters prior to adding new characters so there are no repeat buttons
+  $('#buttonsView').empty();
+
+  // loops through the charactres array
+  for (var i = 0; i < characters.length; i++){
+
+  	// dynamically makes buttons for every shwo in the array
+  	var a = $('<button>')
+  	a.addClass('character'); // add a class
+  	a.attr('data-name', characters[i]); // add a data-attribute
+  	a.text(characters[i]); // make button text
+  	$('#buttonsView').append(a); // append the button to buttonsView div
   }
-});
-
-function characterButtonClicked() {
-  var userInput = $('#character-input').val();
-  searchGif(userInput);
 }
 
-function submitButtonClicked() {
-  var userInput = $('#character-input').val();
+// handles addCharacter button event
+$("#addCharacter").on("click", function() {
+  
+  // grabs the user character input
+  var character = $("#character-input").val().trim();
 
-  if(userInput) {
-  	$('#character-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput +"'> " + userInput + " </button>");
-  }
-}
+  // that input is now added to the array
+  characters.push(character);
 
-function searchGif(gifName) {
-  $.ajax({
-  	url: 'https://api.giphy.com/v1/gifs/search?q= ' + gifName + ' &api_key=dc6zaTOxFJmzC',
-  	type: 'GET',
-  })
-  .done(function(response) {
-  	displayGif(response);
-  })
-}
+  // the makeButtons function is called, which makes buttons for all characters plus the user character
+  makeButtons();
 
-function displayGif(response) {
-  $('#characters').empty();
-  for (var i = 0; i < response.data.length; i++) {
-  	var rating = "<div class='ratings'> Rating: " + (response.data[i].rating) + " </div>";
-  	var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url + '"data-still=" ' + response.data[i].images.fixed_height_still.url + ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="movImage" style= "width:250px; height: 250px">';
+  // this line is so users can hit "enter" instead of clicking the submit button
+  return false;
+})
 
-  	image = '<div class="col-md-4">' + image + "</div>";
-  	$('#characters').append(image);
+// function to display gifs
+function displayGifs(){
+  var character = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + character + "&limit=9&api_key=dc6zaTOxFJmzC";
 
-  }
+  // creates ajax call
+  $.ajax({url: queryURL, method: "GET"}).done(function (response) {
+  	console.log(response.data);
 
-  $('.movImage').on('click', function() {
-  	var state = $(this).attr('data-state');
-  	if (state == 'still') {
-  		$(this).attr('src', $(this).attr("data-animate"));
-  		$(this).attr('data-state', 'animate');
-  	} else {
-  		$(this).attr('src', $(this).attr("data-still"));
-  		$(this).attr('data-state', 'still');
+  	// save results as a variable
+  	var results = response.data;
+
+  	// for loop goes through each gif and adds these variables
+  	for (var i = 0; i < results.length; i++) {
+
+  	  // creates a generic div to hold the results
+  	  var gifDiv = $('<div class=gifs>');
+  	  var characterGif = $('<img>');
+  	    characterGif.attr('src', results[i].images.fixed_height_still.url);
+
+  	    // characters the rating on the hover
+  	    characterGif.attr('title', "Rating: " + results[i].rating);
+  	    characterGif.attr('data-still', results[i].images.fixed_height_still.url);
+  	    characterGif.attr('data-state', 'still');
+  	    characterGif.addClass('gif');
+  	    characterGif.attr('data-animate', results[i].images.fixed_height.url);
+
+  	  // var rating = results [i].rating;
+  	  // var p = $('<p>').text('Rating: ' + rating);
+  	  gifDiv.append(characterGif)
+
+  	  // gifDiv.append(p)
+
+  	  $("#gifsView").prepend(gifDiv);
   	}
-  	
   });
 }
+
+// function for animating gifs
+$(document).on('click', '.gif', function(){
+  var state = $(this).attr('data-state');
+    if ( state == 'still'){
+      $(this).attr('src', $(this).data('animate'));
+      $(this).attr('data-state', 'animate');
+    } else {
+      $(this).attr('src', $(this).data('still'));
+      $(this).attr('data-state', 'still');
+    };
+
+});
+
+// function for displaying character gifs
+$(document).on("click", ".character", displayGifs);
+
+// initially calls the makeButtons function
+makeButtons(); 
